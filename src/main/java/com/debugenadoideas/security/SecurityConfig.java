c06304317e05c4dc95986170575edbaefdb1afd3
adpa1982 <adpa363@hotmail.com>
2023-10-05 17:39:56 -0500

    05 cors_csfr

--- src/main/java/com/debugenadoideas/security/SecurityConfig.java
+++ src/main/java/com/debugenadoideas/security/SecurityConfig.java
@@ -13,8 +13,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 /**
  * @author Alberto Puche Algarin
@@ -27,6 +34,10 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        var requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
         http.authorizeHttpRequests(auth ->
                 auth.requestMatchers("/loans/**", "/balance/", "/accounts/", "/cards/")
                         .authenticated()
@@ -34,6 +45,15 @@ public class SecurityConfig {
                         .permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
+
+        http.cors(cors -> corsConfigurationSource());
+
+        http.csrf(csrf -> csrf
+                        .csrfTokenRequestHandler(requestHandler)
+                        .ignoringRequestMatchers("/welcome", "/about_us")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
+
         return http.build();
     }
 
@@ -67,4 +87,28 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }*/
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        var config = new CorsConfiguration();
+
+        // Registrar solo los origins(urls) permitidos
+        //config.setAllowedOrigins(List.of("http://localhost:4200", "http://my-app.com"));
+
+        // Permite cualquier origin
+        config.setAllowedOrigins(List.of("*"));
+
+        // Permite la operacion de solo los metodos permitidos
+        //config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+
+        // Permite la operaciond e todos los metodos
+        config.setAllowedMethods(List.of("*"));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 }
